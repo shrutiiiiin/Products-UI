d06140603d83ad956609493d3f195501c6abbf70
Shruti Naik <111608883+shrutiiiiin@users.noreply.github.com>
2024-08-31 18:34:01 +0530

    product

--- lib/products/product.dart
+++ lib/products/product.dart
@@ -1,40 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:test_1/widgets/prductItem.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({super.key});
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFF9F9F9),
-        appBar: AppBar(
-            toolbarHeight: 110,
-            backgroundColor: Color(0xFFF9F9F9),
-            title: Padding(
-              padding: const EdgeInsets.only(left: 12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Products',
-                    style: TextStyle(
-                        fontSize: 38,
-                        fontFamily: 'Lato',
-                        fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    'Super summer sale',
-                    style: TextStyle(fontSize: 14, fontFamily: 'Lato'),
-                  )
-                ],
-              ),
-            )),
-        body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return ProdItem();
-          },
-        ));
+      backgroundColor: Color(0xFFF9F9F9),
+      appBar: AppBar(
+          toolbarHeight: 110,
+          backgroundColor: Color(0xFFF9F9F9),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Products',
+                  style: TextStyle(
+                      fontSize: 38,
+                      fontFamily: 'Lato',
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  'Super summer sale',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: 'Lato',
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black26),
+                )
+              ],
+            ),
+          )),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ProdItem();
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'Bag'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favorite'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xFFDB3022),
+        unselectedItemColor:
+            Colors.black26, // Set unselected item color to white
+        backgroundColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
--- lib/widgets/prductItem.dart
+++ lib/widgets/prductItem.dart
@@ -9,91 +9,174 @@ class ProdItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: SizedBox(
-              height: 120,
-              width: double.infinity,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white, // White background for the whole container
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(12),
+            bottomRight: Radius.circular(12),
+            topLeft: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+          ), // Border radius for the whole container
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12.withOpacity(0.1),
+              spreadRadius: 0.5,
+              blurRadius: 2,
+              offset: Offset(0, 1), // Shadow position
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: SizedBox(
+                height: 120,
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF9F9F9),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                        ),
+                      ),
+                    ),
+                    Positioned.fill(
+                        child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                      ),
+                      child: Image.asset('assets/images/img1.png',
+                          fit: BoxFit.cover),
+                    )),
+                    Positioned(
+                      top: 10,
+                      left: 10,
+                      child: Container(
+                        width: 54,
+                        height: 23,
+                        decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: const Center(
+                            child: Padding(
+                          padding: EdgeInsets.all(2.0),
+                          child: Text(
+                            '-20 % off',
+                            style: TextStyle(
+                                fontFamily: 'Lato',
+                                fontSize: 12,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        )),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(width: 4),
+            Expanded(
+              flex: 1,
               child: Stack(
                 children: [
+                  // Background Container
                   Container(
-                    width: double.infinity,
-                    height: 120,
                     decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(30),
+                      color: Colors
+                          .green[100], // Background color for illustration
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  Positioned.fill(
-                      child: Image.asset('assets/images/img1.png',
-                          fit: BoxFit.cover)),
-                  Positioned(
-                    top: 10,
-                    left: 10,
-                    child: Container(
-                      width: 54,
-                      height: 23,
-                      decoration: BoxDecoration(
-                          color: Colors.redAccent,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Center(
-                          child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Text(
-                          '20 % off',
-                          style: TextStyle(fontFamily: 'Lato', fontSize: 12),
+                  // Text overlay
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Row(
+                              children: List.generate(
+                                5,
+                                (index) {
+                                  return Icon(
+                                    Icons.star,
+                                    color: Colors.yellow[700],
+                                    size: 20,
+                                  );
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              '(10)',
+                              style: TextStyle(
+                                  fontFamily: 'Lato',
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black26),
+                            ),
+                          ],
                         ),
-                      )),
+                        Text(
+                          'Dorothy Perkins',
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black26,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          'Evening Dress',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              '15\$',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Lato',
+                                  color: Colors.black26,
+                                  decoration: TextDecoration.lineThrough),
+                            ),
+                            SizedBox(
+                              width: 6,
+                            ),
+                            Text(
+                              '10\$',
+                              style: TextStyle(
+                                  fontFamily: 'Lato',
+                                  color: Color(0xFFDB3022),
+                                  fontWeight: FontWeight.w700),
+                            )
+                          ],
+                        )
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          SizedBox(width: 4),
-          Expanded(
-            flex: 1,
-            child: Stack(
-              children: [
-                // Background Container
-                Container(
-                  decoration: BoxDecoration(
-                    color:
-                        Colors.green[100], // Background color for illustration
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                // Text overlay
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Product Name',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'This is a description of the product. It provides more details and information.',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
